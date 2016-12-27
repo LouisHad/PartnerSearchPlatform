204b1ab30875b629c087fe5f31519b589003a3d6
LouisHad <loizoshadjiantoni@gmail.com>
2016-12-27 16:09:28 +0200

    edit settings.py & models.py

--- PartnerSearch/models.py
+++ PartnerSearch/models.py
@@ -1,5 +1,50 @@
 from __future__ import unicode_literals
 
+from django.contrib.auth.models import User
 from django.db import models
 
+
+class Institution(models.Model):
+    institution_name=models.CharField(max_length=500)
+    country=models.CharField(max_length=100)
+    city=models.CharField(max_length=150)
+    erasmusCN=models.CharField(max_length=150) #ERASMUS Charter Number
+    website = models.CharField(max_length=500)
+
+    def __str__(self):
+        return self.institution_name
+
+
+class Programme(models.Model):
+    programmeName=models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.programmeName
+
+
 # Create your models here.
+class UserAttribute(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    institution= models.ForeignKey(Institution, on_delete=models.CASCADE,blank=True,null=True)
+    fullname=models.CharField(max_length=200)
+    contact_email = models.EmailField()
+    contact_phone = models.CharField(max_length=25, blank=True, null=True)
+
+    def __str__(self):
+        return self.fullname
+
+
+class SubmitRequest(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    # institution= models.ForeignKey(Institution, on_delete=models.CASCADE,blank=True,null=True)
+    interest_Expected = models.DateField(help_text="Please use the following format: <em>DD-MM-YYYY</em>.")
+    summary = models.CharField(max_length=500)
+
+    def __str__(self):
+        return self.institution
+
+
+class Interest(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    interest_to = models.ForeignKey(SubmitRequest, on_delete=models.CASCADE)
+
--- PartnerSearchPlatform/settings.py
+++ PartnerSearchPlatform/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'PartnerSearch'
 ]
 
 MIDDLEWARE_CLASSES = [
@@ -77,8 +78,16 @@ WSGI_APPLICATION = 'PartnerSearchPlatform.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'partner_search',
+        'USER': 'root',
+        'PASSWORD': '',
+        'HOST': 'localhost',
+        'POST': '3306',
+        'OPTIONS':{
+            'autocommit':True
+        }
+
     }
 }
 
